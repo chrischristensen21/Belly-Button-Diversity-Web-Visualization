d952cd7ecda8428768216b4555aef2908a1dbbea
Chris Christensen <chrischristensen21@gmail.com>
2021-10-14 22:14:13 -0700

    final final push

--- static/js/app.js
+++ static/js/app.js
@@ -19,18 +19,12 @@ function bellyButton(id) {
     //console.log(otu_labels);
     //console.log(sample_values);
 
-    // For the bar chart just take the first 10
-    // Meaning you need to for each of the 3 arrays you need to do .slice(0,10).reverse()
-    // For the bar chart, x is going to be the sample_values, y is going to be the otu_ids, and text is going to be
-    // otu_labels
     let otu_ids_slice = otu_ids_OTU.slice(0, 10).reverse();
     let otu_labels_slice = otu_labels.slice(0, 10).reverse();
     let sample_values_slice = sample_values.slice(0, 10).reverse();
     //console.log(otu_ids_slice)
     //console.log(otu_labels_slice)
     //console.log(sample_values_slice)
-    // Now I have everything to do plotly
-    // set up the trace, layout, Plotly.newPlot("bar",yourDataName, yourLayoutName)
 
     var barTrace = {
       x: sample_values_slice,
@@ -47,7 +41,6 @@ function bellyButton(id) {
      }
     Plotly.newPlot('bar', barData, layout); 
 
-    //Now do the bubble chart, but it uses all of the data, also don't need to do the .map
     var bubbleTrace = {
       x: otu_ids,
       y: sample_values,
@@ -68,8 +61,6 @@ function bellyButton(id) {
     };
     
     Plotly.newPlot('bubble', bubbleData, layout);
-
-    //Now do the metadata demo panel at sample-metadata
   
    
     let metaDataFiltered = data.metadata.filter(obj => obj.id == id);
